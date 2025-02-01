e15600a9f2c38609eaa7c15c2fd3cd4799af495b
wakewakame <0214.taiki@gmail.com>
2025-02-01 23:12:04 +0900

    デザインの修正

--- src/App.tsx
+++ src/App.tsx
@@ -12,24 +12,22 @@ const App = () => {
     ctx.fillRect(0, 0, 640, 480);
   }, []);
 
-  const options = [
-    { id: 1, name: 'Option 1' },
-    { id: 2, name: 'Option 2' },
-    { id: 3, name: 'Option 3' },
-  ];
+  const options1 = [...Array(3)].map((_, i) => ({ id: i + 1, name: `Option ${i + 1}` }));
+  const options2 = [...Array(10)].map((_, i) => ({ id: i + 1, name: `Option ${i + 1}` }));
+  const options3 = [...Array(100)].map((_, i) => ({ id: i + 1, name: `Optionaaaaaaaaaaaaaaaa ${i + 1}` }));
 
   return (
     <div className="flex flex-col h-dvh">
-      <div className="w-full h-16 flex flex-row gap-4 items-center justify-center">
+      <div className="w-full h-16 py-2 flex flex-row gap-4 items-center justify-center">
         <ButtonSelector icon="monitor" />
-        <ButtonSelector icon="mic" options={options} />
-        <ButtonSelector icon="volume_up" options={options} />
-        <ButtonSelector icon="piano" options={options} />
+        <ButtonSelector icon="mic" options={options1} />
+        <ButtonSelector icon="volume_up" options={options2} />
+        <ButtonSelector icon="piano" options={options3} />
         <ButtonSelector icon="code" onChange={(enable) => setEditor(enable)} />
       </div>
       <div className="w-full h-[calc(100dvh-(var(--spacing)*16))] relative">
         <canvas ref={canvas} width="640" height="480" className="size-full object-contain absolute"></canvas>
-        <div className={`size-full ${editor ? 'block' : 'hidden'}`}>
+        <div className={`size-full ${editor ? "" : "opacity-0 invisible"} transition-all duration-50 ease-in-out`}>
           <Editor
             className="size-full absolute opacity-70"
             defaultLanguage="javascript"
--- src/components/ButtonSelector.tsx
+++ src/components/ButtonSelector.tsx
@@ -23,66 +23,65 @@ const ButtonSelector = ({ icon, options, defaultSelected, onChange }: ButtonSele
   }
 
   return (
-    <div className="flex flex-col"
-      onMouseLeave={() => setOpen(false)}
-    >
-      { (options !== undefined && open) ? (
-        <div className="relative h-0 z-1">
-          <div className="absolute w-32 top-12 left-0">
-            <Options options={options} selected={selected ?? -1} onSelected={onSelected} />
-          </div>
-        </div>
-      ) : null }
+    <div onMouseLeave={() => setOpen(false)} className="flex flex-col h-full">
       <div className="
-        flex flex-row rounded-full bg-gray-300
-        [&:has(>button:first-child:hover)]:bg-gray-200 transition-all duration-150 ease-in-out
+        flex flex-row h-full rounded-full bg-zinc-700/50
+        [&:has(>button:first-child:hover)]:bg-zinc-700/80 transition-all duration-150 ease-in-out
       ">
         { (options !== undefined) ? (
           <button onClick={() => setOpen(!open)}
             className="
-              w-8 h-12 rounded-l-full bg-gray-800/0
-              text-gray-700 hover:text-gray-500 cursor-pointer transition-all duration-150 ease-in-out
+              relative aspect-2/3 h-full rounded-l-full bg-zinc-800/0
+              text-zinc-300 cursor-pointer transition-all duration-150 ease-in-out
             "
           >
-            <span className="material-icons ml-2 my-3 text-6">arrow_drop_down</span>
+            <span className="material-icons absolute top-1/2 right-0 -translate-y-1/2">arrow_drop_down</span>
           </button>
         ) : null }
         <button onClick={onClick}
           className={`
-            w-12 h-12 rounded-full ${enable ? "bg-blue-500" : "bg-gray-100"} cursor-pointer
-            hover:bg-sky-500 transition-all duration-150 ease-in-out
+            relative aspect-1/1 h-full rounded-full text-zinc-100 cursor-pointer
+            ${enable ? "bg-blue-400 hover:bg-blue-300" : "bg-zinc-700 hover:bg-zinc-600"} transition-all duration-150 ease-in-out
           `}
         >
-          <span className={`
-            material-icons m-3 text-6 ${enable ? "text-gray-100" : "text-gray-700"}
-            transition-all duration-150 ease-in-out
-          `}>{icon}</span>
+          <span className={`material-icons absolute top-1/2 left-1/2 -translate-1/2`}>
+            {icon}
+          </span>
         </button>
       </div>
+      { (options !== undefined) ? (
+        <div className="relative w-full h-0 z-1">
+          <div className="absolute w-2/1 pt-1 top-0 left-1/2 -translate-x-1/2">
+            <Options options={options} open={open} selected={selected ?? -1} onSelected={onSelected} />
+          </div>
+        </div>
+      ) : null }
     </div>
   );
 };
 
 type OptionsArgs = {
   options: { id: number, name: string }[];
+  open: boolean;
   selected: number;
   onSelected: (i: number) => void;
 };
 
-const Options = ({ options, selected, onSelected }: OptionsArgs) => {
+const Options = ({ options, open, selected, onSelected }: OptionsArgs) => {
   return (
-    <div className="flex flex-col">
-      {options.map((option, i) => (
-        <button key={option.id}
-          onClick={() => onSelected(i)}
-          className={`
-            px-2 py-0 ${(i === selected) ? "bg-blue-500" : "bg-gray-200"} text-left truncate whitespace-nowrap cursor-pointer
-            hover:bg-gray-400 transition-all duration-150 ease-in-out
-          `}
-        >
-          <span className="text-1xl text-gray-800">{option.name}</span>
-        </button>
-      ))}
+    <div className={`${open ? "max-h-40" : "max-h-0 opacity-0 invisible"} rounded-md bg-zinc-700 shadow-xl overflow-scroll transition-all duration-150 ease-in-out`}>
+      <div className="flex flex-col">
+        {options.map((option, i) => (
+          <button key={option.id} onClick={() => onSelected(i)}
+            className={`
+              px-2 rounded-md text-left break-words text-1xl text-zinc-100 cursor-pointer
+              ${(i === selected) ? "bg-blue-400 hover:bg-blue-300" : "hover:bg-zinc-600"} transition-all duration-150 ease-in-out
+            `}
+          >
+            <p>{option.name}</p>
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
