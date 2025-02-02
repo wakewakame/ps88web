57476cf2d553d9e51818eba366060019e2df1aaf
wakewakame <0214.taiki@gmail.com>
2025-02-02 18:21:11 +0900

    ある程度動くようになった

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,93 @@ import { useState } from 'react';
 import Editor from '@monaco-editor/react';
 import { ButtonSelector, Option } from './components/ButtonSelector';
 import { Canvas } from './components/Canvas';
-import { AudioDevices } from './controller/AudioDevices';
+import { AudioController } from './controller/AudioController';
 
 const App = () => {
   const [inputs, setInputs] = useState<Option[]>([]);
   const [outputs, setOutputs] = useState<Option[]>([]);
   const [editor, setEditor] = useState<boolean>(false);
 
-  const getInputs = () => {
-    AudioDevices.getInputs().then(setInputs);
-  };
-  const getOutputs = () => {
-    AudioDevices.getOutputs().then(setOutputs);
-  };
-  const setInput = (enable: boolean, id?: string) => {
-    AudioDevices.setInput(enable, id);
-  };
-  const setOutput = (enable: boolean, id?: string) => {
-    AudioDevices.setOutput(enable, id);
+  const defaultCode =
+`console.log('Hello, World!');
+let wave = [];
+let count = 0;
+const audio = (ctx) => {
+  wave = wave.concat([...ctx.audio[0]]).slice(-1024);
+  for (ch = 0; ch < ctx.audio.length; ch++) {
+    for (i = 0; i < ctx.audio[ch].length; i++) {
+      if (ch === 0) {
+        count += 1;
+        ctx.audio[ch][i] = Math.sin(440 * 2 * Math.PI * count / 44100) * 0.01;
+      } else {
+        ctx.audio[ch][i] = ctx.audio[0][i];
+      }
+    }
+  }
+  //console.log(wave.length);
+};
+const gui = (ctx, area, mouse) => {
+  //console.log(wave[0]);
+  const w = [...Array(area.width)].map((_, x) => {
+    const wi = Math.floor(wave.length * x / area.width);
+    const y = wave[wi] * 500.0 + area.height * 0.5;
+    return [x, y];
+  }).flat();
+  ctx.shapes.push({"Polygon": {
+    "shape": w,
+    "stroke": 0xFFFFFF,
+  }});
+};
+return { audio, gui };
+`;
+  const [code, setCode] = useState<string>(defaultCode);
+  const [build, setBuild] = useState<number | undefined>();
+  const onCodeChange = (code?: string) => {
+    if (build !== undefined) {
+      clearTimeout(build);
+    }
+    if (code === undefined) {
+      return;
+    }
+    setBuild(setTimeout(() => {
+      AudioController.setProcessor(code);
+      setCode(code);
+    }, 1000));
   };
 
-  const options = [...Array(5)].map((_, i) => ({ id: `${i + 1}`, name: `Option ${i + 1}` }));
+  const getInputs = () => {
+    AudioController.getInputs()
+      .then((devices) => devices.map((device) => ({
+        id: device.deviceId,
+        name: device.deviceName,
+      })))
+      .then(setInputs);
+  };
+  const getOutputs = () => {
+    AudioController.getOutputs()
+      .then((devices) => devices.map((device) => ({
+        id: device.deviceId,
+        name: device.deviceName,
+      })))
+      .then(setOutputs);
+  };
+  const setInput = async (enable: boolean, id?: string) => {
+    await AudioController.setInput(enable, id);
+    AudioController.setProcessor(code);
+  };
+  const setOutput = async (enable: boolean, id?: string) => {
+    await AudioController.setOutput(enable, id);
+    AudioController.setProcessor(code);
+  };
+  const dummyMidis = [...Array(5)].map((_, i) => ({ id: `${i + 1}`, name: `Option ${i + 1}` }));
+
+  const onMouse = (event: "up" | "down" | "move", x: number, y: number) => {
+    AudioController.setMouse(event, x, y);
+  };
+  const onDraw = () => {
+    AudioController.draw();
+    return AudioController.getShapes();
+  };
 
   return (
     <div className="flex flex-col h-dvh">
@@ -30,16 +96,17 @@ const App = () => {
         <ButtonSelector icon="monitor" />
         <ButtonSelector icon="mic" options={inputs} onOpen={getInputs} onChange={setInput} />
         <ButtonSelector icon="volume_up" options={outputs} onOpen={getOutputs} onChange={setOutput} />
-        <ButtonSelector icon="piano" options={options} />
+        <ButtonSelector icon="piano" options={dummyMidis} />
         <ButtonSelector icon="code" onChange={(enable) => setEditor(enable)} />
       </div>
       <div className="w-full h-[calc(100dvh-(var(--spacing)*16))] relative">
-        <Canvas width={640} height={480}></Canvas>
+        <Canvas width={640} height={480} onMouse={onMouse} onDraw={onDraw}></Canvas>
         <div className={`size-full ${editor ? "" : "opacity-0 invisible"} transition-all duration-100 ease-in-out`}>
           <Editor
+            onChange={onCodeChange}
             className="size-full absolute opacity-70"
             defaultLanguage="javascript"
-            defaultValue="// Write your code here"
+            defaultValue={defaultCode}
             theme="vs-dark"
           />
         </div>
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -3,9 +3,11 @@ import { useEffect, useRef } from 'react';
 type CanvasArgs = {
   width: number;
   height: number;
+  onMouse?: (event: "up" | "down" | "move", x: number, y: number) => void;
+  onDraw?: () => any[];
 };
 
-export const Canvas = ({ width, height }: CanvasArgs) => {
+export const Canvas = ({ width, height, onMouse, onDraw }: CanvasArgs) => {
   const canvas = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
     const canvasElem = canvas.current;
@@ -19,7 +21,12 @@ export const Canvas = ({ width, height }: CanvasArgs) => {
       const yOffset = xFit ? (canvasElem.clientHeight - height / scale) / 2 : 0;
       mouse = { x: scale * (e.offsetX - xOffset), y: scale * (e.offsetY - yOffset) };
     };
-    canvasElem.addEventListener('mousemove', getMousePosition);
+    const mouseUp = () => { onMouse?.('up', mouse.x, mouse.y); };
+    const mouseDown = () => { onMouse?.('down', mouse.x, mouse.y); };
+    const mouseMove = (event: MouseEvent) => { getMousePosition(event); onMouse?.('move', mouse.x, mouse.y); };
+    canvasElem.addEventListener('mouseup', mouseUp);
+    canvasElem.addEventListener('mousedown', mouseDown);
+    canvasElem.addEventListener('mousemove', mouseMove);
 
     const ctx = canvasElem.getContext('2d')!;
     let ref: null | number = null;
@@ -30,12 +37,43 @@ export const Canvas = ({ width, height }: CanvasArgs) => {
       ctx.arc(mouse.x, mouse.y, 20, 0, 2 * Math.PI);
       ctx.fillStyle = '#eee';
       ctx.fill();
+      const shapes = (onDraw?.() ?? []);
+      shapes.map((shape) => Object.entries(shape)).forEach((elem) => elem.map(([key, value]: any) => {
+        if (key === "Polygon") {
+          if (value.shape !== undefined) {
+            ctx.beginPath();
+            for (let i: number = 0; i < value.shape.length / 2; i++) {
+              const x = value.shape[i * 2];
+              const y = value.shape[i * 2 + 1];
+              if (i === 0) {
+                ctx.moveTo(x, y);
+              } else {
+                ctx.lineTo(x, y);
+              }
+            }
+          }
+          if (value.fill !== undefined) {
+            ctx.fillStyle = `#${value.fill.toString(16).padStart(6, '0')}`;
+            ctx.fill();
+          }
+          if (value.stroke !== undefined) {
+            ctx.strokeStyle = `#${value.stroke.toString(16).padStart(6, '0')}`;
+            ctx.lineWidth = value.stroke_width ?? 1;
+            ctx.stroke();
+          }
+        }
+        if (key === "Text") {
+          // TODO
+        }
+      }));
       ref = requestAnimationFrame(loop);
     };
     loop();
 
     return () => {
-      canvasElem.removeEventListener('mousemove', getMousePosition);
+      canvasElem.removeEventListener('mouseup', mouseUp);
+      canvasElem.removeEventListener('mousedown', mouseDown);
+      canvasElem.removeEventListener('mousemove', mouseMove);
       if (ref !== null) cancelAnimationFrame(ref);
     };
   }, [width, height]);
